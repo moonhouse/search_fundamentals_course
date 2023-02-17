d46fbd7e6a0629ab3c23852212527f575b1d99e5
David Hall <david@dpg.se>
2023-02-17 17:31:48 +0000

    Project 2

--- week2/search.py
+++ week2/search.py
@@ -5,6 +5,8 @@ from flask import (
     Blueprint, redirect, render_template, request, url_for, current_app
 )
 
+import json
+
 from week2.opensearch import get_opensearch
 
 import week2.utilities.query_utils as qu
@@ -57,15 +59,18 @@ def process_filters(filters_input):
 
 @bp.route('/autocomplete', methods=['GET'])
 def autocomplete():
+    opensearch = get_opensearch()
     results = {}
     if request.method == 'GET':  # a query has been submitted
         prefix = request.args.get("prefix")
         print(f"Prefix: {prefix}")
         if prefix is not None:
-            type = request.args.get("type", "queries") # If type == queries, this is an autocomplete request, else if products, it's an instant search request.
+            # This next line looked a bit too susceptible to injection to leave alone.
+            index = "bbuy_products" if request.args.get("type", "queries") == "products" else "bbuy_queries" # If type == queries, this is an autocomplete request, else if products, it's an instant search request.
             ##### W2, L3, S1
-            search_response = None
-            print("TODO: implement autocomplete AND instant search")
+            query_obj = { "suggest": { "autocomplete": { "prefix": prefix, "completion": { "field": "suggest", "skip_duplicates": True } } } }
+            print(json.dumps(query_obj))
+            search_response = opensearch.search(body=query_obj, index=index)
             if (search_response and search_response['suggest']['autocomplete'] and search_response['suggest']['autocomplete'][0]['length'] > 0): # just a query response
                 results = search_response['suggest']['autocomplete'][0]['options']
     print(f"Results: {results}")
@@ -106,9 +111,13 @@ def query():
 
         query_obj = qu.create_query(user_query,  [], sort, sortDir, size=20)  # We moved create_query to a utility class so we could use it elsewhere.
         ##### W2, L1, S2
+        if prior_clicks is not None:
+            qu.add_click_priors(query_obj, user_query, prior_clicks)
 
         ##### W2, L2, S2
-        print("Plain ol q: %s" % query_obj)
+        qu.add_spelling_suggestions(query_obj, user_query)
+
+        print("Plain ol q:\n %s" % json.dumps(query_obj))
     elif request.method == 'GET':  # Handle the case where there is no query or just loading the page
         user_query = request.args.get("query", "*")
         filters_input = request.args.getlist("filter.name")
@@ -121,8 +130,11 @@ def query():
             (filters, display_filters, applied_filters) = process_filters(filters_input)
         query_obj = qu.create_query(user_query,  filters, sort, sortDir, size=20)
         #### W2, L1, S2
+        if prior_clicks is not None:
+            qu.add_click_priors(query_obj, user_query, prior_clicks)
 
         ##### W2, L2, S2
+        qu.add_spelling_suggestions(query_obj, user_query)
 
     else:
         query_obj = qu.create_query("*", "", [], sort, sortDir, size=100)
